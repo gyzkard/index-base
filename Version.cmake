167490ba2631a48c305345fa7a6ad6a7a8433076
gyzkard <gyzkard@gyzkard.com>
2021-04-14 13:06:50 +0200

    fix c/p typo

--- Version.cmake
+++ Version.cmake
@@ -1,4 +1,4 @@
-# To update the range-v3 version, from a working directory that is clean except for an
+# To update the index-base version, from a working directory that is clean except for an
 # updated doc/release_notes.md file, update the version numbers below. This makefile will
 # generate a new version.hpp, *AMEND THE MOST RECENT COMMIT*, and git-tag the commit.
 
